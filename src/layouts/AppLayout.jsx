712607a1f00ba178e6d19b736f5a738c60c50f96
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-21 17:25:28 +0530

    feat(src/layouts/AppLayout.jsx): create footer

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -3,12 +3,15 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
 	return (
 		<>
-			<main>
+			<main className="min-h-screen container m-auto">
 				{/* header */}
 				{/* body */}
 				<Outlet />
 			</main>
 			{/* footer */}
+			<div className="p-10 text-center bg-gray-800 mt-10">
+				<footer>All rights reserved! &copy; Shortify Pvt.Ltd</footer>
+			</div>
 		</>
 	);
 };
