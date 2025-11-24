16d1507e03f0a56ee9906f816b088ad5e2342c09
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-24 20:11:41 +0530

    feat(src/pages/Landing.jsx): add hero heading in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Landing = () => {
-	return <div>Landing</div>;
+	const [longURL, setLongURL] = useState("");
+
+	const navigate = useNavigate();
+
+	const handleShorten = (e) => {
+		e.preventDefault();
+		if (longURL) navigate(`/auth?createNew=${longURL}`);
+	};
+
+	return (
+		<div className="flex flex-col items-center">
+			<h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
+				The only URL Shortener <br /> you&rsquo;ll ever need! 👇
+			</h2>
+		</div>
+	);
 };
 export default Landing;
