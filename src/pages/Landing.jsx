6ed646775cb3c6e6bf6b9735f9d608b627e5f66d
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-25 20:12:32 +0530

    feat(src/pages/Landing.jsx): add hero form in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -18,6 +18,21 @@ const Landing = () => {
 			<h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
 				The only URL Shortener <br /> you&rsquo;ll ever need! 👇
 			</h2>
+			<form
+				onSubmit={handleShorten}
+				className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2"
+			>
+				<Input
+					type="url"
+					placeholder="Enter your loooong URL"
+					value={longURL}
+					onChange={(e) => setLongURL(e.target.value)}
+					className="h-full flex-1 py-4 px-4"
+				/>
+				<Button type="submit" className="h-full" variant="destructive">
+					Shorten!
+				</Button>
+			</form>
 		</div>
 	);
 };
