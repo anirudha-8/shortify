35658fc76ea6eabe3bbecd490d014f250f85f3ae
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-14 12:45:00 +0530

    feat(src/components/Input.jsx): handling input change for login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,8 +11,19 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { Error } from "./Error";
+import { BeatLoader } from "react-spinners";
 
 const Login = () => {
+	// handling input
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevState) => ({ ...prevState, [name]: value }));
+	};
+
 	return (
 		<Card>
 			<CardHeader>
@@ -23,16 +34,28 @@ const Login = () => {
 			</CardHeader>
 			<CardContent className="space-y-2">
 				<div className="space-y-1">
-					<Input type="email" name="email" placeholder="Enter email" />
+					<Input
+						type="email"
+						name="email"
+						placeholder="Enter email"
+						onChange={handleInputChange}
+					/>
 					<Error message={"Invalid Email"} />
 				</div>
 				<div className="space-y-1">
-					<Input type="password" name="password" placeholder="Enter password" />
+					<Input
+						type="password"
+						name="password"
+						placeholder="Enter password"
+						onChange={handleInputChange}
+					/>
 					<Error message={"Invalid Password"} />
 				</div>
 			</CardContent>
 			<CardFooter>
-				<Button>Login</Button>
+				<Button>
+					{true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+				</Button>
 			</CardFooter>
 		</Card>
 	);
