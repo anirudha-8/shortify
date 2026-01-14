77afe66b3476d9c721df88d39cc35da72ca4e55c
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-14 12:30:19 +0530

    feat(src/components/login.jsx): create basic structure of login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,37 @@
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Login</CardTitle>
+				<CardDescription>
+					to your account if you already have one
+				</CardDescription>
+			</CardHeader>
+			<CardContent className="space-y-2">
+				<div className="space-y-1">
+					<Input type="email" name="email" placeholder="Enter email" />
+				</div>
+				<div className="space-y-1">
+					<Input type="password" name="password" placeholder="Enter password" />
+				</div>
+			</CardContent>
+			<CardFooter>
+				<Button>Login</Button>
+			</CardFooter>
+		</Card>
+	);
 };
 export default Login;
