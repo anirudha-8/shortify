84d07146298f169d9763ee7284656fb488e02464
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-16 08:23:09 +0530

    fix(src/hooks/useFetch.js): fix issue in useFetch.js custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-const useFetch = (cb, optoins = {}) => {
+const useFetch = (cb, options = {}) => {
 	const [data, setData] = useState(null);
-	const [loading, setLoading] = useState(null);
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
 	const fn = async (...args) => {
 		setLoading(true);
 		setError(null);
 		try {
-			const response = await cb(optoins, ...args);
+			const response = await cb(options, ...args);
 			setData(response);
 			setError(null);
 		} catch (error) {
