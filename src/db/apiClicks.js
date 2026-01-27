08cf613149fda06dd6b719ea816acf525ae95e28
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-27 18:10:49 +0530

    fix(src/db/apiClicks.js): fix getClicksForUrls api logic

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,14 +1,14 @@
-import supabase from "supabase";
+import supabase from "./supabase";
 
 export async function getClicksForUrls(urlIds) {
 	const { data, error } = await supabase
 		.from("clicks")
 		.select("*")
-		.in("urlId", urlIds);
+		.in("url_id", urlIds);
 
 	if (error) {
 		console.error(error.message);
-		throw new Error("Unable to load clicks!");
+		return null;
 	}
 	return data;
 }
