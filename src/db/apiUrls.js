31744d5ea51c705630a51a64a77e030d9bbef498
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-03 22:32:56 +0530

    feat(src/db/apiUrls.js): add API for creating URL

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
 	console.log("Fetching URLs for user ID:", user_id);
@@ -25,6 +25,43 @@ export async function deleteUrl(id) {
 	if (error) {
 		console.error(error.message);
 		throw new Error("Unable to delete URL");
+	}
+
+	return data;
+}
+
+export async function createUrl(
+	{ title, longUrl, customUrl, user_id },
+	qrCode,
+) {
+	// create text for short URL
+	const shortUrl = Math.random().toString(36).substring(2, 6);
+	const filename = `qr-${shortUrl}`;
+
+	const { error: storageError } = await supabase.storage
+		.from("qrs")
+		.upload(filename, qrCode);
+	if (storageError) throw new Error(storageError.message);
+
+	const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${filename}`;
+
+	const { data, error } = await supabase
+		.from("urls")
+		.insert([
+			{
+				title,
+				original_url: longUrl,
+				custom_url: customUrl || null,
+				user_id,
+				short_url: shortUrl,
+				qr,
+			},
+		])
+		.select();
+
+	if (error) {
+		console.error(error.message);
+		throw new Error("Error creating short URL");
 	}
 
 	return data;
