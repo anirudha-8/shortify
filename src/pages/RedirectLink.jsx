3d1b54522d274a4f9fcef62e5b4d8f1ae4930b07
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-06 23:27:28 +0530

    feat(src/pages/RedirectLink.jsx): create logic for redirecting to url using short link

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,4 +1,40 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { getLongUrl } from "../db/apiUrls";
+import { storeClicks } from "../db/apiClicks";
+import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
+
 const RedirectLink = () => {
-	return <div>RedirectLink</div>;
+	const { id } = useParams();
+
+	const { loading, data, fn: fnGetLongUrl } = useFetch(getLongUrl, id);
+
+	const { loading: loadingStats, fn: fnStats } = useFetch(storeClicks, {
+		id: data?.id,
+		original_url: data?.original_url,
+	});
+
+	useEffect(() => {
+		fnGetLongUrl();
+	}, []);
+
+	useEffect(() => {
+		if (!loading && data) {
+			fnStats();
+		}
+	}, [loading]);
+
+	if (loading || loadingStats) {
+		return (
+			<>
+				<BarLoader width={"100%"} color="#36d7b7" />
+				<br />
+				Redirecting...
+			</>
+		);
+	}
+
+	return null;
 };
 export default RedirectLink;
