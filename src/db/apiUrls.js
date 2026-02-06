26b891289f8470b45de3435a048af58878015611
Anirudha Bele <beleanirudha8@gmail.com>
2026-02-06 23:26:41 +0530

    fix(src/db/apiUrls.js): fix logic for selecting id and original_url

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -68,16 +68,16 @@ export async function createUrl(
 }
 
 export async function getLongUrl(id) {
-	const { data, error } = await supabase
+	let { data: shortLinkData, error: shortLinkError } = await supabase
 		.from("urls")
-		.select("id", "original_url")
-		.or(`short_url.eq.${id}, custom_url.eq.${id}`)
+		.select("id, original_url")
+		.or(`short_url.eq.${id},custom_url.eq.${id}`)
 		.single();
 
-	if (error) {
-		console.error(error.message);
-		throw new Error("Error fetching short link!");
+	if (shortLinkError && shortLinkError.code !== "PGRST116") {
+		console.error("Error fetching short link:", shortLinkError);
+		return;
 	}
 
-	return data;
+	return shortLinkData;
 }
